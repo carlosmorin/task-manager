c2be084927d7a9dda55a03e08a9378258053c4d3
carlosmorin78 <carlosmorin78@gmail.com>
2023-07-28 10:16:46 -0600

    controller specs

--- spec/lib/json_web_token_spec.rb
+++ spec/lib/json_web_token_spec.rb
@@ -5,13 +5,6 @@ RSpec.describe JsonWebToken do
   let(:user) { create :user, password: "123213123"}
   let(:token) { token_generator(user.id) }
 
-  describe ".encode" do
-    it "returns valid token" do
-      encoded = subject.encode(user_id: user.id)
-      expect(encoded.length).to eq(107)
-    end
-  end
-
   describe ".decode" do
     it "returns payload content" do
       decoded = subject.decode(token)
